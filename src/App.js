cb3649b6d653947168868e3bf07f9e9ce310025d
balthazarely <balthazar2120@gmail.com>
2020-10-21 14:37:18 -0600

    fetch working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://code-challenge.spectrumtoolbox.com/api/restaurants`, {
+      headers: {
+        Authorization: "Api-Key q3MNxtfep8Gt ",
+      },
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        setData(json);
+      });
+  }, []);
+
+  return <div></div>;
 }
 
 export default App;
