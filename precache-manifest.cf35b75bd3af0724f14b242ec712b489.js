9470f56ff13f1da2b8f247a960433da9d059b081
balthazarely <balthazar2120@gmail.com>
2020-10-22 11:09:17 -0600

    Updates

---
+++ precache-manifest.cf35b75bd3af0724f14b242ec712b489.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc3f3a2cf377541e2c52dfe47dbc8aed",
+    "url": "/code-challenge/index.html"
+  },
+  {
+    "revision": "9c315ba9f7ae733735ba",
+    "url": "/code-challenge/static/css/main.06687c7c.chunk.css"
+  },
+  {
+    "revision": "dd3339c77b1bc24d577c",
+    "url": "/code-challenge/static/js/2.8993c131.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/code-challenge/static/js/2.8993c131.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c315ba9f7ae733735ba",
+    "url": "/code-challenge/static/js/main.1f76fb85.chunk.js"
+  },
+  {
+    "revision": "70f4d9deeef316ee9093",
+    "url": "/code-challenge/static/js/runtime-main.836d88eb.js"
+  }
+]);
